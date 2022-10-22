d7c6c30fdde8235e363d5a5f9c12f816e2e4188e
VaclavPilat <pilatvacl@seznam.cz>
2022-10-22 17:23:12 +0200

    Added colored prefix

--- Progtest.sh
+++ Progtest.sh
@@ -9,6 +9,10 @@ SEPARATOR () {
     printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -;
 } ;
 
+GETCOLOR () {
+    echo -e "\033[0;3$((1 + $1 % 5))m";
+} ;
+
 TESTCASE () {
     if [ -d $2 ];
     then
@@ -23,8 +27,8 @@ TESTCASE () {
                 OUTPUT_DIFFERENCE=`diff "$OUTPUT_FILE" "$TEMPORARY_FILE_NAME"`;
                 if [ ! $? -eq 0 ];
                 then
-                    echo "$1: Successfully tried $SUCCESSFUL_TEST_COUNT/$MAXIMUM_TEST_COUNT inputs.";
-                    echo "$1: Failed test on $INPUT_FILE";
+                    echo "$1 Successfully tried $SUCCESSFUL_TEST_COUNT/$MAXIMUM_TEST_COUNT inputs.";
+                    echo "$1 Failed test on $INPUT_FILE";
                     SEPARATOR;
                     cat "$INPUT_FILE";
                     SEPARATOR;
@@ -35,7 +39,7 @@ TESTCASE () {
                 SUCCESSFUL_TEST_COUNT=$((SUCCESSFUL_TEST_COUNT+1));
             fi;
         done;
-        echo "$1: Successfully tried $SUCCESSFUL_TEST_COUNT $2 inputs.";
+        echo "$1 Successfully tried $SUCCESSFUL_TEST_COUNT $2 inputs.";
     fi;
 } ;
 
@@ -48,38 +52,41 @@ COMPILE () {
     diff "$HASH_FILE_NAME" "$TEMPORARY_FILE_NAME" > /dev/null 2> /dev/null;
     if [ ! $? -eq 0 ] || [ ! -f "$COMPILED_FILE_NAME" ];
     then
-        echo "$1: Compiling source code ...";
+        echo "$1 Compiling source code ...";
         g++ -Wall -pedantic "$SOURCE_FILE_NAME" -o "$COMPILED_FILE_NAME" || exit 1;
         md5sum "$SOURCE_FILE_NAME" > "$HASH_FILE_NAME";
         md5sum "$COMPILED_FILE_NAME" >> "$HASH_FILE_NAME";
     else
-        echo "$1: Skipped compilation (not needed).";
+        echo "$1 Skipped compilation (not needed).";
     fi;
 } ;
 
 RUNTESTS () {
-    cd $1;
+    cd $2;
+    PREFIX="$(GETCOLOR $1)$2$(echo -e '\033[0m'):";
     if [ ! -f $SOURCE_FILE_NAME ];
     then
-        echo "$1: $SOURCE_FILE_NAME not found!";
+        echo "$PREFIX $SOURCE_FILE_NAME not found!";
         return;
     fi;
-    COMPILE $1;
-    echo "$1: Testing input files ...";
+    COMPILE $PREFIX;
+    echo "$PREFIX Testing input files ...";
     for FOLDER in *;
     do
         if [ -d $FOLDER ];
         then
-            TESTCASE "$1" "$FOLDER" "$INPUT_FILE_SUFFIX" "$OUTPUT_FILE_SUFFIX";
+            TESTCASE "$PREFIX" "$FOLDER" "$INPUT_FILE_SUFFIX" "$OUTPUT_FILE_SUFFIX";
         fi;
     done;
     cd ..;
 } ;
 
+COUNT=1
 for FOLDER in *;
 do
     if [ -d $FOLDER ];
     then
-     RUNTESTS $FOLDER;
+        RUNTESTS $COUNT $FOLDER;
     fi;
+    COUNT=$((COUNT+1));
 done;
